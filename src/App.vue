08be1e69e4213e1e6f780c54e9b068cf150141a5
d-murphy12 <d12murph@gmail.com>
2023-09-24 16:36:47 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,3 +22,63 @@ import HelloWorld from './components/HelloWorld.vue'
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted() {
+    document.querySelector('.duplicate').addEventListener('click', function(e) {
+  const cardToClone = document.querySelector('.card').cloneNode(true);
+  document.body.appendChild(cardToClone);
+});
+ 
+//change background color
+const card = document.querySelector('#card');
+        let backgroundColor = false;
+    document.querySelector('#changebackground').addEventListener('click', function(e) {
+            if (backgroundColor) {
+                card.style.backgroundColor = 'white';
+            } else {
+                card.style.backgroundColor = 'gray';
+            }
+            backgroundColor = !backgroundColor;
+        });
+    
+   //change title button
+document.querySelector('.changetxt').addEventListener('click', function(e) {
+  let name = "Something else";
+  if (name) {
+    document.querySelector('.card h3').innerText = name;
+  }
+});
+    
+    //delete button
+document.querySelector('#deletecard').addEventListener('click', function(e) {
+  let wantsTo = confirm("Are you sure?");
+  if (wantsTo) {
+    if (document.querySelector('.card:last-child') !== document.querySelector('.card')) {
+      document.querySelector('.card:last-child').remove();      
+    }
+    else {
+      alert("Cannot original last card");
+    }
+  }
+});
+    
+    //toggle button
+const details = document.querySelector('summary');
+
+document.querySelector('button[data-toggle-btn]').addEventListener('click', (e) => {
+  if (details.parentNode.getAttribute('open')) {
+        details.parentNode.removeAttribute('open');
+  }
+  else {
+    details.parentNode.setAttribute('open','open');    
+  }
+});
+  }
+}
+</script>
